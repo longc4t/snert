97f42b4df96a6d9048ad0f7f1c348d0328239200
WuTo <1015047222@qq.com>
2019-05-01 23:10:26 +0800

    add js

---
+++ model/article.py
@@ -0,0 +1,8 @@
+from conn import sqlop
+from config import jsonify
+
+class article(object):
+    def __init__(self):
+        self.cur=sqlop()
+    def insertarticle(self):
+        pass
--- static/static/js/main.js
+++ static/static/js/main.js
@@ -1,4 +1,4 @@
-layer = layui.layer
+layer = layui.layer;
 if (Cookies.get('token')){
     $(".personal")[0].href="/user"
 }else {
@@ -9,7 +9,7 @@ function check_login() {
 	var pass = $("#password").val();
 	if (name =="" || pass == "") {
         $("#login_form").removeClass('shake_effect');
-	    setTimeout(function(){$("#login_form").addClass('shake_effect')},1)
+	    setTimeout(function(){$("#login_form").addClass('shake_effect')},1);
 	    layer.alert('请填写完整', {
 			icon: 2,
 			skin: 'layer-ext-moon'
@@ -17,11 +17,11 @@ function check_login() {
     }else{
         $.post("/api/login",JSON.stringify({"username":btoa(name),"password": btoa(pass)}),function (results){
             if(results.success){
-                Cookies.set("token",results.token)
+                Cookies.set("token",results.token);
                 layer.alert(results.msg, {
 			        icon: 1,
 			        skin: 'layer-ext-moon'
-		        })
+		        });
 		        window.location.href="/"
             }else{
                 layer.alert(results.msg, {
@@ -38,7 +38,7 @@ function check_register(){
 	var pass = $("#r_password").val();
 	if(name =="" || pass==""){
 	    $("#login_form").removeClass('shake_effect');
-	    setTimeout(function(){$("#login_form").addClass('shake_effect')},1)
+	    setTimeout(function(){$("#login_form").addClass('shake_effect')},1);
 	    layer.alert('请填写完整', {
 			icon: 2,
 			skin: 'layer-ext-moon'
@@ -46,11 +46,11 @@ function check_register(){
 	}else{
 	    $.post("/api/register",JSON.stringify({"username":btoa(name),"password": btoa(pass)}),function (results){
             if(results.success){
-                Cookies.set("token",results.token)
+                Cookies.set("token",results.token);
                 layer.alert(results.msg, {
 			        icon: 1,
 			        skin: 'layer-ext-moon'
-		        })
+		        });
 		        window.location.href="/"
             }else{
                 layer.alert(results.msg, {
@@ -61,3 +61,84 @@ function check_register(){
         },"json");
     }
 }
+
+function comment() {
+    $.post("/api/comment/add",JSON.stringify({"commentauthor":btoa(author),"commentauthorid": commentid,"commentcontent":btoa(content),"commenttimestamp":timestamp,"token":Cookies.get('token')}),function (results){
+            if(results.success){
+                layer.alert(results.msg, {
+			        icon: 1,
+			        skin: 'layer-ext-moon'
+		        })
+		        //window.location.href="/"
+            }else{
+                layer.alert(results.msg, {
+                    icon: 2,
+                    skin: 'layer-ext-moon'
+                })
+            }
+        },"json");
+}
+
+function getcomment() {
+    $.post("/api/comment/search",JSON.stringify({"commentidarrary":idarrary,"token":Cookies.get('token')}),function (results){
+            if(results.success){
+                layer.alert(results.msg, {
+			        icon: 1,
+			        skin: 'layer-ext-moon'
+		        })
+		        //window.location.href="/"
+            }else{
+                layer.alert(results.msg, {
+                    icon: 2,
+                    skin: 'layer-ext-moon'
+                })
+            }
+        },"json");
+}
+
+function article() {
+    $.post("/api/article/add",JSON.stringify({"articletitle":btoa(title),"articleauthor":btoa(author),"articleauthorid": authorid,"articlecontent":btoa(content),"articletimestamp":timestamp,"token":Cookies.get('token')}),function (results){
+            if(results.success){
+                layer.alert(results.msg, {
+			        icon: 1,
+			        skin: 'layer-ext-moon'
+		        })
+		        //window.location.href="/"
+            }else{
+                layer.alert(results.msg, {
+                    icon: 2,
+                    skin: 'layer-ext-moon'
+                })
+            }
+        },"json");
+}
+
+function getarticle() {
+    $.post("/api/article/index",JSON.stringify({"pagenum":num,"token":Cookies.get('token')}),function (results){
+            if(results.success){
+                html="";
+                for (var i in results.data){
+                    html+="<div class=\"item\"><div class=\"item-box  layer-photos-demo1 layer-pho" +
+                        "tos-demo\"><h3><a href=\"details.html?id="
+                        +results.data[i]["articleid"]
+                        +"\">"
+                        +atob(results.data[i]["articletitle"]).replace("<","&lt;").replace(">","&gt;")
+                        +"</a></h3>"+"<h5>发布于：<span>"
+                        +new Date(results.data[i]["articletimestamp"]*1000).toLocaleString()
+                        +"</span></h5><p>"
+                        +atob(results.data[i]["articlecontent"]).replace("<","&lt;").replace(">","&gt;")
+                        +"</p></div><div class=\"comment count\"><a href=\"details.html?id="
+                        +results.data[i]["commentid"]
+                        +"\">评论</a><a href=\"javascript:;\" class=\"like\">点赞</a></div></div>"
+                }
+		        //window.location.href="/"
+            }else{
+                layer.alert(results.msg, {
+                    icon: 2,
+                    skin: 'layer-ext-moon'
+                })
+            }
+        },"json");
+}
+
+
